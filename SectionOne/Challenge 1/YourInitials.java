8016fc781786a517643a0cbfd5b2ca9cce0e1a59
Gabriella Jakobson <gabriella_jakobson@hotmail.com>
2022-03-01 20:44:31 +0100

    Challenge 1 done

---
+++ SectionOne/Challenge 1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here. 
+        System.out.println(" GGG   JJJJJ");
+        System.out.println("G   G    J");
+        System.out.println("G        J");
+        System.out.println("G GGG    J");
+        System.out.println("G   G    J");
+        System.out.println("G   G  J J");
+        System.out.println(" GGG    JJ");
+
+    }
+}
