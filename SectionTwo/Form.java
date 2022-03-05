a51b4c94fa95fa25b03e0e7609c76741f52c0954
Gabriella Jakobson <gabriella_jakobson@hotmail.com>
2022-03-05 20:21:40 +0100

    Learned about Char and String

---
+++ SectionTwo/Form.java
@@ -0,0 +1,9 @@
+public class Form {
+    public static void main(String[] args) {
+        char gender = 'F';
+        char gender2 = 'M';
+        String sentence = " Gender: ";
+        System.out.println(gender + gender2 + sentence + gender + gender2);
+    }
+    
+}
