a99c042da665b56ad7ee4cb19e4ce23d330189fe
Gabriella Jakobson <gabriella_jakobson@hotmail.com>
2022-03-06 12:14:49 +0100

    int vs long

---
+++ SectionTwo/WholeNumbers.java
@@ -0,0 +1,9 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        int numberOffFans = 5000;
+        System.out.println("There are " + numberOffFans + " fans at the Quidditch games");
+
+        long globalPopulation = 7000000000L;
+        System.out.println("There are " + globalPopulation + " people of earth");
+    }
+}
