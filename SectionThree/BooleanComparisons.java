bdb6b288ec69a565df6f0a94e52054c6487b9741
Gabriella Jakobson <gabriella_jakobson@hotmail.com>
2022-03-11 20:23:25 +0100

    Intro to booleans

---
+++ SectionThree/BooleanComparisons.java
@@ -0,0 +1,21 @@
+public class BooleanComparisons {
+    public static void main(String[] args) {
+        int chemistryGrade = 95;
+        int biologyGrade = 75;
+
+        boolean comparison1 = biologyGrade > chemistryGrade;
+
+        System.out.println(comparison1);
+
+        double sales = 37.55;
+        double costs = 5.55;
+
+        System.out.println(sales > costs);
+
+        String sentence = "I love this course";
+        String sentence2 = "I love this course";
+        System.out.println(!sentence.equals(sentence2));
+
+    }
+    
+}
