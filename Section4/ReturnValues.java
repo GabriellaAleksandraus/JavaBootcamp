3a30b7c705474a29ab1364483d189d56925017e8
Gabriella Jakobson <gabriella_jakobson@hotmail.com>
2022-03-13 18:23:21 +0100

    Switch, if and else in a function

--- Section4/ReturnValues.java
+++ Section4/ReturnValues.java
@@ -1,28 +1,39 @@
 public class ReturnValues {
     public static void main(String[] args) {
 
-        double length = 3.1;
+        double length = -3.1;
         double width = 2.3;
-        double area = measureRectangle(length, width);
-        stringPrinter(length, width, area);
+        String option = "area";
+        double area = measureRectangle(length, width, option);
+        stringPrinter(length, width, area, "area");
 
-        length = 4.1;
-        width = 5.3;
-        area = measureRectangle(length, width);
-        stringPrinter(length, width, area);
+        length = 3.1;
+        width = 2.3;
+        option = "perimeter";
+        area = measureRectangle(length, width, option);
+        stringPrinter(length, width, area, "perimeter");
 
 
     }
 
 
-    public static double measureRectangle(double length, double width) {
-        double area = length * width;
+    public static double measureRectangle(double length, double width, String option) {
+        if (length < 0 || width <0) {
+            System.out.println("Length or width cannot be negative");
+            System.exit(0);
+        }
+        switch (option){
+            case "area": return length * width;
+            case "perimeter": return 2 * (length * width);
+            default: return 404;
+        }
+        //double area = length * width;
         //System.out.println(length + " * " + width + " = " + area + "\n");
-        return area;
+        //return area;
     }
 
-    public static void stringPrinter(double length, double width, double area) {
-        System.out.println(length + " * " + width + " = " + area + "\n");
+    public static void stringPrinter(double length, double width, double area, String option) {
+        System.out.println(option + ": " + length + " * " + width + " = " + area + "\n");
 
     }
 
