075376bb7c38d1fbbea454ebcfa4dd1e3d0092ef
Gabriella Jakobson <gabriella_jakobson@hotmail.com>
2022-03-30 21:24:46 +0200

    fix

--- Section6/ArraysAnsLoops.java
+++ Section6/ArraysAnsLoops.java
@@ -8,7 +8,11 @@ public class ArraysAnsLoops {
            System.out.println("The element at index " + i + " is " + kingdoms[i]);
        }
 
-       ArrayList<String> kingdoms2 = new ArrayList<>("M", "W", "N", "E");
+       ArrayList<String> kingdoms2 = new ArrayList<>();
+       kingdoms2.add("M");
+       kingdoms2.add("W");
+       kingdoms2.add("N");
+       kingdoms2.add("E");
        System.out.println("The number of elements is " + kingdoms2.size());
        for (int i = 0; i < kingdoms2.size(); i++) {
            System.out.println("The element at index " + i + " is " + kingdoms2.get(i));
