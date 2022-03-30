138f5194a0469844fe1ea73398dbb042bc23e8a8
Gabriella Jakobson <gabriella_jakobson@hotmail.com>
2022-03-30 19:55:54 +0200

    arrays intro + workbook 6.1 done

---
+++ Section6/IntroToArrays.java
@@ -0,0 +1,11 @@
+public class IntroToArrays {
+    public static void main(String[] args) {
+        String[] kingdoms = {"Mercia", "Wes", "North", "EA"};
+        System.out.println(kingdoms[0]);
+        System.out.println(kingdoms[1]);
+        System.out.println(kingdoms[2]);
+        System.out.println(kingdoms[3]);
+
+    }
+    
+}
